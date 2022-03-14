1f424c41e2510699ac9c55281d3584246c7a1606
Piotr Orczykowski <orczykowski.peter@gmail.com>
2022-03-14 17:29:09 +0100

    Minor changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,12 +3,12 @@
     <h1 id="title">todos</h1>
 
     <div id="placeholder">
-      <img src="@/assets/to-do-list.png" alt="check-list" width="80px">
+      <img src="@/assets/to-do-list.png" alt="check-list">
       <h2>Add your first todo</h2>
       <h3>What do you want to get done today?</h3>
     </div>
     
-    <form id="newToDoForm" action="">
+    <form id="newToDoForm">
       <label for="newToDo"></label>
       <input type="text" id="newToDo" name="newToDo" placeholder="E.g. Buy something">
     </form>
@@ -29,18 +29,17 @@ export default {
   }
 
   #app {
-    height: 90vh;
+    height: 95vh;
     display: flex;
     flex-direction: column;
-    margin-top: 10vh;
+    margin-top: 5vh;
   }
-
 
   #title {
     align-self: center;
     width: auto;
 
-    font-size: 6em;
+    font-size: 5em;
     color: gray;
   }
 
@@ -48,30 +47,36 @@ export default {
     align-self: center;
     text-align: center;
 
-    padding: 2em;
+    padding: 1em;
   }
 
   #placeholder h2 {
-    font-size: 2em;
+    font-size: 1.5em;
     margin-top: 0.5em;
   }
 
   #placeholder h3 {
+    font-size: 1em;
     margin-top: 1em;  
   }
 
+  #placeholder img {
+    width: 70px;
+  }
+
   #newToDoForm {
+    margin-top: 1em;  
     align-self: center;
   }
 
   #newToDo {
-    width: 30em;
+    width: 25em;
     height: 1em;
 
     padding: 0.5em;
     font-size: 1.2em;
 
-    border: 4px solid black;
+    border: 2px solid black;
     border-radius: 0.2em;
   }
 
