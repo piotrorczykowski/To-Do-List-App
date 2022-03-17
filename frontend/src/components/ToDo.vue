ab68c18fca65df13e1dc5d8a163a66f377fe2552
Piotr Orczykowski <orczykowski.peter@gmail.com>
2022-03-17 11:37:18 +0100

    Divide app into components

---
+++ frontend/src/components/ToDo.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="todo">
+    <div class="item">
+      <img
+        class="circle"
+        v-bind:src="require('@/assets/circle.png')"
+        alt="Left icon"
+        v-on:click="doneToDo()"
+        ref="circle"
+      />
+      <h2 v-bind:class="{ done: todo.done }">
+        {{ todo.title }}
+      </h2>
+      <img
+        class="cancel"
+        src="@/assets/cancel.png"
+        alt="Cancel"
+        v-on:click="removeToDo()"
+      />
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ["todo"],
+  methods: {
+      removeToDo() {
+        this.$emit('removeClicked', this.todo._id)
+      },
+      doneToDo() {
+          this.$emit('doneClicked', this.todo._id)
+
+          const circleIcon = require("@/assets/circle.png")
+          const checkedIcon = require("@/assets/checked.png")
+          this.todo.done ? this.$refs.circle.src = checkedIcon : this.$refs.circle.src = circleIcon
+      }
+  }
+}
+</script>
+
+
+<style>
+.todo {
+  padding: 0.5em;
+  font-size: 1.2em;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-self: center;
+}
+
+.item {
+  width: 26.5em;
+  height: 2em;
+  display: relative;
+}
+
+.item .circle {
+  position: absolute;
+  left: calc(50vw - 26.5em / 2);
+}
+
+.item h2 {
+  position: absolute;
+  left: calc(50vw - 7em);
+}
+
+.item .cancel {
+  position: absolute;
+  left: calc(50vw + 10.5em);
+}
+
+.done {
+  text-decoration: line-through;
+  color: gray;
+}
+
+.circle,
+.cancel {
+  width: 2em;
+  height: 2em;
+
+  cursor: pointer;
+}
+
+.circle:hover,
+.cancel:hover {
+  transform: scale(1.1);
+  transition-duration: 0.2s;
+}
+
+.cancel:hover {
+  transition-duration: 0.1s;
+  filter: invert(18%) sepia(92%) saturate(6493%) hue-rotate(356deg)
+    brightness(87%) contrast(100%);
+}
+</style>
---
+++ frontend/src/components/ToDoList.vue
@@ -0,0 +1,135 @@
+<template>
+  <div id="app">
+    <h1 id="title">todos</h1>
+
+    <ToDo
+        v-bind:todo="todo"
+        v-for="todo in todos"
+        v-bind:key="todo._id"
+        v-on:doneClicked="doneToDo"
+        v-on:removeClicked="removeToDo"
+    />
+
+    <div id="placeholder" v-if="!countOfToDos">
+      <img src="@/assets/to-do-list.png" alt="check-list" />
+      <h2>Add your first todo</h2>
+      <h3>What do you want to get done today?</h3>
+    </div>
+
+    <form id="newToDoForm" v-on:submit.prevent>
+      <label for="newToDo"></label>
+      <input
+        id="newToDo"
+        type="text"
+        name="newToDo"
+        autocomplete="off"
+        maxlength="30"
+        placeholder="E.g. Buy something"
+        v-on:keyup.enter="addToDo"
+        v-model="newToDo"
+      />
+    </form>
+  </div>
+</template>
+
+<script>
+import ToDo from "./ToDo.vue";
+
+export default {
+  components: {
+    ToDo
+  },
+  data() {
+    return {
+      newToDo: "",
+      todos: [
+        { title: "Buy something", done: false, _id: Math.random() },
+        { title: "Do something", done: false, _id: Math.random() },
+      ],
+      countOfToDos: 2
+    }
+  },
+  methods: {
+    addToDo() {
+      this.todos.push({
+        title: this.newToDo,
+        done: false,
+        _id: Math.random(),
+      });
+
+      this.newToDo = ""
+      this.countOfToDos++
+    },
+    removeToDo(_id) {
+      const index = this.todos.findIndex((todo) => todo._id === _id)
+      this.todos.splice(index, 1)
+      this.countOfToDos--
+    },
+    doneToDo(_id) {
+      const index = this.todos.findIndex((todo) => todo._id === _id)
+      this.todos[index].done = !this.todos[index].done
+    }
+  }
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Tangerine");
+
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+}
+
+#app {
+  height: 95vh;
+  display: flex;
+  flex-direction: column;
+  margin-top: 5vh;
+}
+
+#title {
+  align-self: center;
+
+  font-size: 5em;
+  color: gray;
+}
+
+#placeholder {
+  align-self: center;
+  text-align: center;
+
+  padding: 1em;
+}
+
+#placeholder h2 {
+  font-size: 1.5em;
+  margin-top: 0.5em;
+}
+
+#placeholder h3 {
+  font-size: 1em;
+  margin-top: 1em;
+}
+
+#placeholder img {
+  width: 70px;
+}
+
+#newToDoForm {
+  margin-top: 1em;
+  align-self: center;
+}
+
+#newToDo {
+  width: 25em;
+  height: 1em;
+
+  padding: 0.5em;
+  font-size: 1.2em;
+
+  border: 2px solid black;
+  border-radius: 0.2em;
+}
+</style>
